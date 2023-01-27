086296900b219bc9935a5d2e134bccd0536328b4
Danilo Silva <danilo.silva@msn.com>
2023-01-27 18:11:11 -0300

    Criando um projeto com testes

---
+++ EstacionamentoAlura/EstacionamentoAlura.Tests/VeiculoTests.cs
@@ -0,0 +1,24 @@
+using EstacionamentoAlura.App.Modelos;
+using Xunit;
+
+namespace EstacionamentoAlura.Tests
+{
+    public class VeiculoTests
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Acelerar(10);
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
