70241aeb2c821092452d47cb244bbc64013c6ef0
Danilo Silva <danilo.silva@msn.com>
2023-01-27 18:03:51 -0300

    Projeto Inicial

---
+++ EstacionamentoAlura/EstacionamentoAlura.App/Modelos/TipoVeiculo.cs
@@ -0,0 +1,9 @@
+﻿namespace EstacionamentoAlura.App.Modelos
+{
+    //Enumeração para classificar os veículos em carros ou motos.
+    public enum TipoVeiculo
+    {
+        Automovel=0,
+        Motocicleta=1
+    }
+}
