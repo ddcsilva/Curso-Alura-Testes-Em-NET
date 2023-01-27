1abdbd05b55ef824ee669d7b2e044d0b7856d8b5
Danilo Silva <danilo.silva@msn.com>
2023-01-27 18:15:59 -0300

    Testes unitários e o padrão AAA

--- EstacionamentoAlura/EstacionamentoAlura.Tests/VeiculoTests.cs
+++ EstacionamentoAlura/EstacionamentoAlura.Tests/VeiculoTests.cs
@@ -3,21 +3,36 @@ using Xunit;
 
 namespace EstacionamentoAlura.Tests
 {
+    // Padrão dos Testes
+    // Arrange -> Preparação do cenário necessário para invocar o método testado
+    // Act -> Método a ser testado
+    // Assert -> Verificação do resultado obtido após a execução do método testado
+
     public class VeiculoTests
     {
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            // Arrange
             var veiculo = new Veiculo();
+
+            // Act
             veiculo.Acelerar(10);
+
+            // Assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaVeiculoFrear()
         {
+            // Arrange
             var veiculo = new Veiculo();
+
+            // Act
             veiculo.Frear(10);
+
+            // Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
     }
