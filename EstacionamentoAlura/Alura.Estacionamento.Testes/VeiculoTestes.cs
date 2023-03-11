87ebf6bd282853b29740997c65d7b0acda633170
Danilo Silva <danilo.silva@msn.com>
2023-03-11 15:01:44 -0300

    Padrão AAA

--- EstacionamentoAlura/Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ EstacionamentoAlura/Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -1,13 +1,34 @@
+using Alura.Estacionamento.Modelos;
 using Xunit;
 
 namespace Alura.Estacionamento.Testes
 {
     public class VeiculoTestes
     {
-        [Fact]
-        public void Test1()
-        {
+        // A - Arrange -> Preparar o cenário para o teste
+        // A - Act -> O método que será testado
+        // A - Assert -> O resultado esperado
 
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            // Arrange
+            var veiculo = new Veiculo();
+            // Act
+            veiculo.Acelerar(10);
+            // Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            // Arrange
+            var veiculo = new Veiculo();
+            // Act
+            veiculo.Frear(10);
+            // Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
     }
 }
